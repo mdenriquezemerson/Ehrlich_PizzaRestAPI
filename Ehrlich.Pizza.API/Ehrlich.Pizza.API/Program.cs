4ace977078cc5c6c840518fa941577569c6fcbea
mdenriquezemerson <163973999+mdenriquezemerson@users.noreply.github.com>
2024-09-07 14:19:32 +0800

    Initial .net8 web api project creation.
    Commit for an empty web api project with initial swagger docs. Deleted unneeded files and defined the folder structure.

---
+++ Ehrlich.Pizza.API/Ehrlich.Pizza.API/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Pizza Place API",
+        Version = "v1",
+        Description = "REST API for Pizza Place's Database"
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
