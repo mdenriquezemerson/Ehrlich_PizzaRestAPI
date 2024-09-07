e22fe0fd3460ec5dd81c579465d70baa92581560
mdenriquezemerson <163973999+mdenriquezemerson@users.noreply.github.com>
2024-09-07 20:48:11 +0800

    GetOrders full implementation
    Gave GetOrders.Query default values instead of making it nullable. Adjusted the Query Sanitation. Added filtering with Date and Time included.

--- Ehrlich.Pizza.API/Ehrlich.Pizza.API/Providers/OrdersProvider.cs
+++ Ehrlich.Pizza.API/Ehrlich.Pizza.API/Providers/OrdersProvider.cs
@@ -19,10 +19,26 @@ namespace Ehrlich.Pizza.API.Providers
 
         public async Task<GetOrders.Response> GetOrdersAsync(GetOrders.Query query)
         {
+            List<Order> orders = new List<Order> { };
+
+            //OrderId is unique meaning they're only querying for one item and we should return just that if OrderId exists in the query.
+            if (query.OrderId.HasValue)
+            {
+                var orderItem = await _context.Orders.Where(o => o.OrderId == query.OrderId).ToListAsync();
+                return new GetOrders.Response()
+                {
+                    Orders = orders
+                };
+            }
+
+            //Rest of code if OrderId is not specified and multiple items are expected.
             query = SanitizeQuery(query);
-            int PN = query.PN ?? default(int);
-            int PS = query.PS ?? default(int);
-            var orders = await _context.Orders.Skip((PN - 1) * PS).Take(PS).ToListAsync();
+            var orderQuery = _context.Orders.AsQueryable();
+            orderQuery = orderQuery.Where(o => o.Date >= DateOnly.FromDateTime(query.StartDate) && o.Date <= DateOnly.FromDateTime(query.EndDate));
+            orderQuery = orderQuery.Where(o => o.Time >= TimeOnly.Parse(query.StartTime) && o.Time <= TimeOnly.Parse(query.EndTime));
+            orderQuery = orderQuery.Skip((query.PN - 1) * query.PS).Take(query.PS);
+            orders = await orderQuery.ToListAsync();
+            //orders = await _context.Orders.Skip((query.PN - 1) * query.PS).Take(query.PS).ToListAsync();
             return new GetOrders.Response()
             {
                 Orders = orders,
@@ -31,8 +47,19 @@ namespace Ehrlich.Pizza.API.Providers
 
         public GetOrders.Query SanitizeQuery(GetOrders.Query query)
         {
+            TimeOnly endTime;
+            TimeOnly startTime;
+            //TODO: Timezone not clarified. Specify with stakeholders.
+            //If no value provided, set Page Size to 1000
+            query.PS = query.PS <= 0 ? 1000 : query.PS;
+            //If no value provided, set Page Number to 1
+            query.PN = query.PN <= 0 ? 1 : query.PN;
+            //If no value provided, set the end time to time now
+            query.EndTime = !TimeOnly.TryParse(query.EndTime, out endTime) ? DateTime.Now.ToString("HH:mm:ss") : query.EndTime;
+            //if no value provided, set the start time to midnight
+            query.StartTime = !TimeOnly.TryParse(query.StartTime, out startTime) ? "00:00:00" : query.StartTime;
+
             return query;
         }
-
     }
 }
--- Ehrlich.Pizza.API/Ehrlich.Pizza.API/Requests/GetOrders.cs
+++ Ehrlich.Pizza.API/Ehrlich.Pizza.API/Requests/GetOrders.cs
@@ -8,12 +8,18 @@ namespace Ehrlich.Pizza.API.Requests
         public class Query : Response
         {
             public long? OrderId { get; set; }
-            public DateTime? StartDate { get; set; }
-            public DateTime? EndDate { get; set; }
-            public string? StartTime { get; set; }
-            public string? EndTime { get; set; }
-            public int? PS { get; set; } = 1000;
-            public int? PN { get; set; } = 1;
+            //If no value provided, set the start date to minimum possible value
+            public DateTime StartDate { get; set; } = DateTime.MinValue;
+            //If no value provided, set the end date to today.
+            public DateTime EndDate { get; set; } = DateTime.Today;
+            //if no value provided, set the start time to midnight
+            public string StartTime { get; set; } = "00:00:00";
+            //If no value provided, set the end time to time now
+            public string EndTime { get; set; } = DateTime.Now.ToString("HH:mm:ss");
+            //If no value provided, set Page Size to 1000
+            public int PS { get; set; } = 1000;
+            //If no value provided, set Page Number to 1
+            public int PN { get; set; } = 1;
         }
 
         public class Response
